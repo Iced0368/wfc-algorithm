fe46268c9b902237129d8c80e19224764b385195
Iced0368 <iced03682@gmail.com>
2023-03-14 23:12:11 +0900

    _

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
-import random
+import random, time
 from PIL import Image
 from wfc import *
 
-tiles = get_tiles('sample/Flowers.png', 5, 5)
+s = time.time()
+tiles = get_tiles('sample/Flowers.png', (4, 4), flip_horizontal=True)
+print(time.time()-s)
 
-print(tiles.keys())
+s = time.time()
+adj = get_adjacent_tiles(tiles)
+print(time.time()-s)
 
-im = Image.fromarray(random.choice(list(tiles.values())))
-plt.imshow(im)
+tile_hash = random.choice(list(tiles.keys()))
+
+plt.imshow(tiles[tile_hash])
 plt.show()
+
+for adj_tile_hash in adj[tile_hash]['bottom']:
+    plt.imshow(tiles[adj_tile_hash])
+    plt.show()
--- wfc.py
+++ wfc.py
@@ -2,13 +2,64 @@ import numpy as np
 from PIL import Image
 import hashlib
 
-def get_tiles(image_file, width, height):
+def hash_tile(tile):
+    return hashlib.md5(tile.tobytes()).hexdigest()
+
+def get_tiles(image_path, tile_size, flip_horizontal=False, flip_vertical=False, rotate=False):
+    image = Image.open(image_path)
     tiles = {}
-    image = Image.open(image_file)
-    img_width, img_height = image.size
-    for x in range(0, img_width - width + 1, width):
-        for y in range(0, img_height - height + 1, height):
-            tile = image.crop((x, y, x+width, y+height))
-            tile_hash = hashlib.md5(tile.tobytes()).hexdigest()
-            tiles[tile_hash] = np.array(tile)
+    width, height = image.size
+
+    for i in range(0, width - tile_size[0] + 1):
+        for j in range(0, height - tile_size[1] + 1):
+            box = (i, j, i+tile_size[0], j+tile_size[1])
+            tile_img = image.crop(box)
+
+            if flip_horizontal:
+                tile_img_h = tile_img.transpose(Image.FLIP_LEFT_RIGHT)
+                tile_hash_h = hash_tile(tile_img_h)
+                tiles[tile_hash_h] = np.array(tile_img_h)
+
+            if flip_vertical:
+                tile_img_v = tile_img.transpose(Image.FLIP_TOP_BOTTOM)
+                tile_hash_v = hash_tile(tile_img_v)
+                tiles[tile_hash_v] = np.array(tile_img_v)
+
+            if rotate:
+                for k in range(3):
+                    tile_img_r = tile_img.rotate((k+1)*90)
+                    tile_hash_r = hash_tile(tile_img_r)
+                    tiles[tile_hash_r] = np.array(tile_img_r)
+
+            tile_hash = hash_tile(tile_img)
+            tiles[tile_hash] = np.array(tile_img)
+
     return tiles
+
+
+def get_adjacent_tiles(tiles):
+    adjacent_tiles = {}
+    for tile_hash, tile in tiles.items():
+        # Initialize the list of adjacent tiles for this tile
+        adjacent_tiles[tile_hash] = {'top': [], 'bottom': [], 'left': [], 'right': []}
+
+        # Iterate over all other tiles to check for adjacency
+        for other_hash, other_tile in tiles.items():
+            if tile_hash != other_hash:
+                # Check if other tile can be placed on top of this tile
+                if np.array_equal(tile[0, :], other_tile[-1, :]):
+                    adjacent_tiles[tile_hash]['top'].append(other_hash)
+
+                # Check if other tile can be placed below this tile
+                if np.array_equal(tile[-1, :], other_tile[0, :]):
+                    adjacent_tiles[tile_hash]['bottom'].append(other_hash)
+
+                # Check if other tile can be placed to the left of this tile
+                if np.array_equal(tile[:, 0], other_tile[:, -1]):
+                    adjacent_tiles[tile_hash]['left'].append(other_hash)
+
+                # Check if other tile can be placed to the right of this tile
+                if np.array_equal(tile[:, -1], other_tile[:, 0]):
+                    adjacent_tiles[tile_hash]['right'].append(other_hash)
+
+    return adjacent_tiles
